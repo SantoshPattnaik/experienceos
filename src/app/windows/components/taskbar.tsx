e0e66303234fddb40c46191955dac784c59ca072
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-05-23 21:59:04 +0530

    sent footer to a component

---
+++ src/app/windows/components/taskbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer className="bg-zinc-50 text-center dark:bg-neutral-700 fixed w-full bottom-0">
+			<div className="bg-black/5 p-4 text-center text-surface dark:text-white">
+				<div>Taskbar</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
--- src/app/windows/page.tsx
+++ src/app/windows/page.tsx
@@ -1,17 +1,11 @@
 import React from "react";
+import Footer from "./components/taskbar";
 
 const page = () => {
 	return (
 		<React.StrictMode>
-			{/* <div className="bg-slate-700 text-white w-full flex justify-between flex-row flex-wrap px-12 pt-8">
-				Taskbar
-			</div> */}
-			<div className="windows h-full">
-				<footer className="bg-zinc-50 text-center dark:bg-neutral-700 fixed w-full bottom-0">
-					<div className="bg-black/5 p-4 text-center text-surface dark:text-white">
-						<div>Taskbar</div>
-					</div>
-				</footer>
+			<div className="windows">
+				<Footer />
 			</div>
 		</React.StrictMode>
 	);
