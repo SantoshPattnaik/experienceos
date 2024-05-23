6aca9a78c4d51608ca6128f5730a6b1f0534aa79
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-05-23 22:47:20 +0530

    date status shifted to right and replaced apple logo

--- src/app/macos/components/Menubar.tsx
+++ src/app/macos/components/Menubar.tsx
@@ -1,30 +1,35 @@
 import React from "react";
+import { FaApple } from "react-icons/fa";
 
 function Menubar() {
-  return (
-    <div className="menubar flex w-full gap-20">
-      <div className="menus">
-        <ul className="flex gap-5 text-[18px] font-medium">
-          <li className="hover:cursor-pointer">🍎</li>
-          <li className="hover:cursor-pointer">Finder</li>
-          <li className="hover:cursor-pointer">File</li>
-          <li className="hover:cursor-pointer">Edit</li>
-          <li className="hover:cursor-pointer">View</li>
-          <li className="hover:cursor-pointer">Go</li>
-          <li className="hover:cursor-pointer">Window</li>
-          <li className="hover:cursor-pointer">Help</li>
-        </ul>
-      </div>
-      <div className="status">
-        <ul className="flex gap-5 text-[18px] font-medium justify-end">
-          <li className="hover:cursor-pointer"></li>
-          <li className="hover:cursor-pointer"></li>
-          <li className="hover:cursor-pointer"></li>
-          <li className="hover:cursor-pointer">{Date()}</li>
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<nav className="menubar flex w-full gap-20">
+			<div className="menus">
+				<ul className="flex gap-5 text-[18px] font-medium">
+					<li className="hover:cursor-pointer">
+						<ul className="text-2xl">
+							<FaApple />
+						</ul>
+					</li>
+					<li className="hover:cursor-pointer">Finder</li>
+					<li className="hover:cursor-pointer">File</li>
+					<li className="hover:cursor-pointer">Edit</li>
+					<li className="hover:cursor-pointer">View</li>
+					<li className="hover:cursor-pointer">Go</li>
+					<li className="hover:cursor-pointer">Window</li>
+					<li className="hover:cursor-pointer">Help</li>
+				</ul>
+			</div>
+			{/* <div className="status"> */}
+			<ul className="flex space-x-36 text-[18px] font-medium justify-end">
+				<li className="hover:cursor-pointer"></li>
+				<li className="hover:cursor-pointer"></li>
+				<li className="hover:cursor-pointer"></li>
+				<li className="hover:cursor-pointer">{Date()}</li>
+			</ul>
+			{/* </div> */}
+		</nav>
+	);
 }
 
 export default Menubar;
