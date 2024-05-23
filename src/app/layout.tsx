047c997a7289b965ba9058ed6d29173c37d98a6f
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-05-23 21:05:15 +0530

    merge conflict testing ends here

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,7 @@ const RootLayout = ({
 }>) => {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}Testingsdsdsds</body>
-			<body className={inter.className}>{children}Testingsdsdsds</body>
+			<body className={inter.className}>{children}</body>
 		</html>
 	);
 };
