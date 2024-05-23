dd5a95faded6bdeb3dfa8c57448b481bc5449020
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-05-23 21:56:35 +0530

    started adding footer as taskbar and increased div height for temporary viewing purpose

--- src/app/windows/page.tsx
+++ src/app/windows/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const page = () => {
-	return <div>Windows page</div>;
+	return (
+		<React.StrictMode>
+			{/* <div className="bg-slate-700 text-white w-full flex justify-between flex-row flex-wrap px-12 pt-8">
+				Taskbar
+			</div> */}
+			<div className="windows h-full">
+				<footer className="bg-zinc-50 text-center dark:bg-neutral-700 fixed w-full bottom-0">
+					<div className="bg-black/5 p-4 text-center text-surface dark:text-white">
+						<div>Taskbar</div>
+					</div>
+				</footer>
+			</div>
+		</React.StrictMode>
+	);
 };
 
 export default page;
