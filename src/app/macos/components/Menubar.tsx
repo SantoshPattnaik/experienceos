f13079a562ae7b2afb7fd7660689d985781384fd
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-05-23 23:03:43 +0530

    added transparency to macos menubar

--- src/app/macos/components/Menubar.tsx
+++ src/app/macos/components/Menubar.tsx
@@ -3,32 +3,34 @@ import { FaApple } from "react-icons/fa";
 
 function Menubar() {
 	return (
-		<nav className="menubar flex w-full gap-20">
-			<div className="menus">
-				<ul className="flex gap-5 text-[18px] font-medium">
-					<li className="hover:cursor-pointer">
-						<ul className="text-2xl">
-							<FaApple />
-						</ul>
-					</li>
-					<li className="hover:cursor-pointer">Finder</li>
-					<li className="hover:cursor-pointer">File</li>
-					<li className="hover:cursor-pointer">Edit</li>
-					<li className="hover:cursor-pointer">View</li>
-					<li className="hover:cursor-pointer">Go</li>
-					<li className="hover:cursor-pointer">Window</li>
-					<li className="hover:cursor-pointer">Help</li>
-				</ul>
-			</div>
-			<div className="status">
-				<ul className="flex space-x-36 text-[18px] font-medium justify-end">
-					<li className="hover:cursor-pointer"></li>
-					<li className="hover:cursor-pointer"></li>
-					<li className="hover:cursor-pointer"></li>
-					<li className="hover:cursor-pointer">{Date()}</li>
-				</ul>
-			</div>
-		</nav>
+		<div className="bg-black/35 text-white">
+			<nav className="menubar flex w-full gap-20">
+				<div className="menus">
+					<ul className="flex gap-5 text-[18px] font-medium">
+						<li className="hover:cursor-pointer">
+							<ul className="text-2xl">
+								<FaApple />
+							</ul>
+						</li>
+						<li className="hover:cursor-pointer">Finder</li>
+						<li className="hover:cursor-pointer">File</li>
+						<li className="hover:cursor-pointer">Edit</li>
+						<li className="hover:cursor-pointer">View</li>
+						<li className="hover:cursor-pointer">Go</li>
+						<li className="hover:cursor-pointer">Window</li>
+						<li className="hover:cursor-pointer">Help</li>
+					</ul>
+				</div>
+				<div className="status">
+					<ul className="flex space-x-36 text-[18px] font-medium justify-end">
+						<li className="hover:cursor-pointer"></li>
+						<li className="hover:cursor-pointer"></li>
+						<li className="hover:cursor-pointer"></li>
+						<li className="hover:cursor-pointer">{Date()}</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
 	);
 }
 
--- src/app/macos/page.tsx
+++ src/app/macos/page.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Menubar from "./components/Menubar";
 
 const page = () => {
-  return (
-    <div className="macos h-[100vh] w-[100w]">
-		<Menubar />
-    </div>
-  );
+	return (
+		<div className="macos h-[100vh] w-[100w]">
+			<Menubar />
+		</div>
+	);
 };
 
 export default page;
