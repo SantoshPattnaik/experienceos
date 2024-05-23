3c8af799ab1dedb37f97577d5be808fb4f684d9d
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-05-23 23:37:12 +0530

    corrected component's name from footer to Taskbar

--- src/app/windows/components/taskbar.tsx
+++ src/app/windows/components/taskbar.tsx
@@ -2,16 +2,15 @@ import React from "react";
 
 import Image from "next/image";
 
-const Footer = () => {
+const Taskbar = () => {
 	return (
 		<footer className="text-center fixed w-full bottom-0">
 			<div className="p-4 text-center text-surface text-white backdrop-blur-md bg-black backdrop-opacity-85 bg-opacity-70 flex">
-				<div className="starmenu flex-grow">
-				</div>
+				<div className="starmenu flex-grow"></div>
 				<div className="w-[25vw]">{Date()}</div>
 			</div>
 		</footer>
 	);
 };
 
-export default Footer;
+export default Taskbar;
--- src/app/windows/page.tsx
+++ src/app/windows/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Footer from "./components/taskbar";
+import Taskbar from "./components/taskbar";
 
 const page = () => {
-  return (
-    <div className="windows w-[100vw] h-[100vh]">
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="windows w-[100vw] h-[100vh]">
+			<Taskbar />
+		</div>
+	);
 };
 
 export default page;
