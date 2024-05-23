7878c99d6db500a7f39f32bbbc4b8d7e58851445
Alter2k20 <96902776+Alter2k20@users.noreply.github.com>
2024-05-23 23:00:28 +0530

    smoll change

--- src/app/windows/components/taskbar.tsx
+++ src/app/windows/components/taskbar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+import Image from "next/image";
+
 const Footer = () => {
 	return (
 		<footer className="text-center fixed w-full bottom-0">
-			<div className="p-4 text-center text-surface text-white backdrop-blur-md backdrop-opacity-85 bg-opacity-45">
-				<div>Taskbar</div>
+			<div className="p-4 text-center text-surface text-white backdrop-blur-md bg-black backdrop-opacity-85 bg-opacity-70 flex">
+				<div className="starmenu flex-grow">
+				</div>
+				<div className="w-[25vw]">{Date()}</div>
 			</div>
 		</footer>
 	);
