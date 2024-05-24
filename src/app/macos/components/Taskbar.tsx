2ac28634bdc7c79681299340066ae99b3f7eeb17
Alter2k20 <96902776+Alter2k20@users.noreply.github.com>
2024-05-24 21:38:30 +0530

    Added some bascic styling, icons to macos and windwos

--- src/app/macos/components/Taskbar.tsx
+++ src/app/macos/components/Taskbar.tsx
@@ -1,10 +1,34 @@
+import Image from "next/image";
 import React from "react";
+
+import calender from "../../../../public/macosicons/cal.png";
+import downloads from "../../../../public/macosicons/downloads.png";
+import finder from "../../../../public/macosicons/finder.png";
+import folder from "../../../../public/macosicons/folder.png";
+import xcode from "../../../../public/macosicons/xcode.png";
 
 function Taskbar() {
   return (
-    <div className=" bg-white/55  w-1/3 h-[60px] self-center rounded-[25px] mb-1">
-      <div className="p-[5px] flex justify-center pt-4">
-        Taskbar Items Here!
+    <div className=" bg-white/55  w-fit h-[60px] self-center rounded-[25px] mb-2 pr-3 pl-3">
+      <div className=" flex justify-center">
+        <ul className="flex">
+          {/*add more icons here */}
+          <li>
+            <Image src={calender} alt="" width={64} height={64} />
+          </li>
+          <li>
+            <Image src={downloads} alt="" width={64} height={64} />
+          </li>
+          <li>
+            <Image src={xcode} alt="" width={64} height={64} />
+          </li>
+          <li>
+            <Image src={finder} alt="" width={64} height={64} />
+          </li>
+          <li>
+            <Image src={folder} alt="" width={64} height={64} />
+          </li>
+        </ul>
       </div>
     </div>
   );
--- src/app/windows/components/taskbar.tsx
+++ src/app/windows/components/taskbar.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-
 import Image from "next/image";
 
+import winstart from "../../../../public/windows/winstartdark.svg";
+
 const Taskbar = () => {
-	return (
-		<footer className="text-center fixed w-full bottom-0">
-			<div className="p-4 text-center text-surface text-white backdrop-blur-md bg-black backdrop-opacity-85 bg-opacity-70 flex">
-				<div className="starmenu flex-grow"></div>
-				<div className="w-[25vw]">{Date()}</div>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="text-center fixed w-full bottom-0 ">
+      <div className="p-4 text-center text-surface text-white backdrop-blur-md bg-black backdrop-opacity-85 bg-opacity-70 flex">
+        <div className="starmenu flex-grow">
+          <Image src={winstart} alt="" height={64} width={64} />
+        </div>
+        <div className="w-[25vw]">{Date()}</div>
+      </div>
+    </footer>
+  );
 };
 
 export default Taskbar;
