452d0359184da68dda15ca0a13d3530b1161bfef
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-05-24 21:34:05 +0530

    wifi, bluetooth icon added

--- src/app/macos/components/Menubar.tsx
+++ src/app/macos/components/Menubar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { FaApple } from "react-icons/fa";
+import { FaApple, FaWifi } from "react-icons/fa";
 import Time from "./Time";
+import { IoMdBluetooth } from "react-icons/io";
 
 function Menubar() {
 	return (
@@ -11,6 +12,7 @@ function Menubar() {
 						<ul className="flex gap-5 text-[17px] font-medium">
 							<li className="hover:cursor-pointer">
 								<ul className="text-2xl">
+									{/* Apple Logo */}
 									<FaApple />
 								</ul>
 							</li>
@@ -24,14 +26,35 @@ function Menubar() {
 						</ul>
 					</div>
 					<div className="status justify-end flex-grow mr-1">
-						<ul className="flex space-x-36 text-[17px] font-medium justify-end ">
-							<li className="hover:cursor-pointer"></li>
-							<li className="hover:cursor-pointer"></li>
-							<li className="hover:cursor-pointer"></li>
+						<ul className="flex space-x-2 text-[17px] font-medium justify-end ">
+							{/* Time Viewer */}
+							<FaWifi className="text-2xl" />
+							<IoMdBluetooth className="text-2xl" />
 							<Time />
 						</ul>
 					</div>
 				</nav>
+			</div>
+			<div className="absolute bg-white/50 top-7 text-center rounded left-2 p-1">
+				<ul>
+					<li>About</li>
+					<li>Shutdown</li>
+					<li>Restart</li>
+				</ul>
+			</div>
+			<div className="absolute bg-white/50 top-7 text-center rounded left-20 p-1">
+				<ul>
+					<li>About</li>
+					<li>Shutdown</li>
+					<li>Restart</li>
+				</ul>
+			</div>
+			<div className="absolute bg-white/50 top-7 text-center rounded left-2 p-1">
+				<ul>
+					<li>About</li>
+					<li>Shutdown</li>
+					<li>Restart</li>
+				</ul>
 			</div>
 			<div className="absolute bg-white/50 top-7 text-center rounded left-2 p-1">
 				<ul>
