09c71261e458a33c84b9116cf3a379accb85ab20
Alter2k20 <mishraashutosh98611@gmail.com>
2024-05-25 20:47:05 +0530

    styled windows a little more

--- src/app/windows/components/Desktop.tsx
+++ src/app/windows/components/Desktop.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Taskbar from "./taskbar";
 
+import folder from "../../../../public/windows/folder.png"
+
+
 function Desktop() {
   return (
     <div>
+      <div className="flex flex-col h-fit"></div>
       <Taskbar />
     </div>
   );
--- src/app/windows/components/taskbar.tsx
+++ src/app/windows/components/taskbar.tsx
@@ -9,7 +9,7 @@ import vs from "../../../../public/windows/Visual Studio.svg";
 import github from "../../../../public/windows/GitHub.svg";
 import blender from "../../../../public/windows/Blender.svg";
 import ae from "../../../../public/windows/After Effects.svg";
-import notification from "../../../../public/windows/bell-svgrepo-com.svg";
+import notification from "../../../../public/windows/notification-bell.png";
 
 
 // Taskbar Component
@@ -23,32 +23,32 @@ const Taskbar = () => {
         <div className="flex-grow flex justify-center">
           <ul className="flex justify-center gap-5 self-center">
             <li className="hover:cursor-pointer">
-              <Image src={winstart} alt="" height={34} width={34} />
+              <Image src={winstart} alt="" height={32} width={32} />
             </li>
             <li className="hover:cursor-pointer">
-              <Image src={vscode} alt="" height={34} width={34} />
+              <Image src={vscode} alt="" height={32} width={32} />
             </li>
             <li className="hover:cursor-pointer">
-              <Image src={github} alt="" height={34} width={34} />
+              <Image src={github} alt="" height={32} width={32} />
             </li>
             <li className="hover:cursor-pointer">
-              <Image src={blender} alt="" height={34} width={34} />
+              <Image src={blender} alt="" height={32} width={32} />
             </li>
             <li className="hover:cursor-pointer">
-              <Image src={vs} alt="" height={34} width={34} />
+              <Image src={vs} alt="" height={32} width={32} />
             </li>
             <li className="hover:cursor-pointer">
-              <Image src={ae} alt="" height={34} width={34} />
+              <Image src={ae} alt="" height={32} width={32} />
             </li>
           </ul>
         </div>
-        <div className="pt-2 hover:cursor-pointer">
+        <div className=" hover:cursor-pointer text-[11px] pt-5">
           <WinTime />
         </div>
-        <div className="flex justify-center align-middle pt-3 pl-1">
+        <div className="flex justify-center align-middle pt-8 pl-2 pr-5">
           <ul className="size-fit">
             <li className="hover:cursor-pointer">
-              <Image src={notification} alt="" height={34} width={34} />
+              <Image src={notification} alt="" height={16} width={16} />
             </li>
           </ul>
         </div>
