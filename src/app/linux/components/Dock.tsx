f56db24f296b20f16ccae7f8ef1527405a400e51
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-05-31 21:38:27 +0530

    shifted linux dock to slight up

--- src/app/linux/components/Dock.tsx
+++ src/app/linux/components/Dock.tsx
@@ -11,34 +11,34 @@ import impress from "../../../../public/linuxicons/libreoffice-impress.svg";
 import writer from "../../../../public/linuxicons/libreoffice-writer.svg";
 
 function Dock() {
-  return (
-    <div className="text-white flex flex-col bg-black/25 h-[400px] w-fit p-3 rounded-2xl mt-56 ml-2 justify-between">
-      <div className="hover:cursor-pointer">
-        <Image src={brave} alt="" height={32} width={32}></Image>
-      </div>
-      <div className="hover:cursor-pointer">
-        <Image src={code} alt="" height={32} width={32}></Image>
-      </div>
-      <div className="hover:cursor-pointer">
-        <Image src={disk} alt="" height={32} width={32}></Image>
-      </div>
-      <div className="hover:cursor-pointer">
-        <Image src={fox} alt="" height={32} width={32}></Image>
-      </div>
-      <div className="hover:cursor-pointer">
-        <Image src={github} alt="" height={32} width={32}></Image>
-      </div>
-      <div className="hover:cursor-pointer">
-        <Image src={kdev} alt="" height={32} width={32}></Image>
-      </div>
-      <div className="hover:cursor-pointer">
-        <Image src={impress} alt="" height={32} width={32}></Image>
-      </div>
-      <div className="hover:cursor-pointer">
-        <Image src={writer} alt="" height={32} width={32}></Image>
-      </div>
-    </div>
-  );
+	return (
+		<div className="text-white flex flex-col bg-black/25 h-[400px] w-fit p-3 rounded-2xl mt-40 ml-2 justify-between">
+			<div className="hover:cursor-pointer">
+				<Image src={brave} alt="" height={32} width={32}></Image>
+			</div>
+			<div className="hover:cursor-pointer">
+				<Image src={code} alt="" height={32} width={32}></Image>
+			</div>
+			<div className="hover:cursor-pointer">
+				<Image src={disk} alt="" height={32} width={32}></Image>
+			</div>
+			<div className="hover:cursor-pointer">
+				<Image src={fox} alt="" height={32} width={32}></Image>
+			</div>
+			<div className="hover:cursor-pointer">
+				<Image src={github} alt="" height={32} width={32}></Image>
+			</div>
+			<div className="hover:cursor-pointer">
+				<Image src={kdev} alt="" height={32} width={32}></Image>
+			</div>
+			<div className="hover:cursor-pointer">
+				<Image src={impress} alt="" height={32} width={32}></Image>
+			</div>
+			<div className="hover:cursor-pointer">
+				<Image src={writer} alt="" height={32} width={32}></Image>
+			</div>
+		</div>
+	);
 }
 
 export default Dock;
