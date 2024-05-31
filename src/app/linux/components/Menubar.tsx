f867b74d94110d389b93a23a094a7833722b1d3b
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-05-31 21:06:11 +0530

    shifted slightly to right

--- src/app/linux/components/Menubar.tsx
+++ src/app/linux/components/Menubar.tsx
@@ -2,19 +2,19 @@ import { LinTime } from "@/app/main_interface/components/Time";
 import React from "react";
 
 function Menubar() {
-  return (
-    <div className="w-full bg-black">
-      <nav>
-        <ul className="text-white flex justify-between">
-          <li className="hover:cursor-pointer">Activities</li>
-          <li className="hover:cursor-pointer">
-            <LinTime />
-          </li>
-          <li className="hover:cursor-pointer"></li>
-        </ul>
-      </nav>
-    </div>
-  );
+	return (
+		<div className="w-full bg-black">
+			<nav>
+				<ul className="text-white flex justify-between">
+					<li className="hover:cursor-pointer pl-2">Activities</li>
+					<li className="hover:cursor-pointer">
+						<LinTime />
+					</li>
+					<li className="hover:cursor-pointer"></li>
+				</ul>
+			</nav>
+		</div>
+	);
 }
 
 export default Menubar;
