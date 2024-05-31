eb3809574a71e6938c6846d54a852e49d34da42d
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-05-31 21:39:13 +0530

    nothing

--- src/app/linux/page.tsx
+++ src/app/linux/page.tsx
@@ -3,16 +3,16 @@ import Dock from "./components/Dock";
 import Menubar from "./components/Menubar";
 
 const page = () => {
-  return (
-    <div className="linux h-[100vh] w-[100vw] flex flex-col">
-      <div>
-        <Menubar />
-      </div>
-      <div className="justify-self-center h-full">
-        <Dock />
-      </div>
-    </div>
-  );
+	return (
+		<div className="linux h-[100vh] w-[100vw] flex flex-col">
+			<div>
+				<Menubar />
+			</div>
+			<div className="justify-self-center h-full">
+				<Dock />
+			</div>
+		</div>
+	);
 };
 
 export default page;
