143fca5e0fe5064f596c4bba1fa7271f2e368d6d
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-05-31 19:48:09 +0530

    added a type to the function component to remove implicit any

--- src/app/windows/page.tsx
+++ src/app/windows/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Desktop from "./components/Desktop";
-const page = () => {
-  return (
-    <div className="windows w-[100vw] h-[100vh]">
-      <Desktop />
-    </div>
-  );
+const page = (): React.JSX.Element => {
+	return (
+		<div className="windows w-[100vw] h-[100vh]">
+			<Desktop />
+		</div>
+	);
 };
 
 export default page;
