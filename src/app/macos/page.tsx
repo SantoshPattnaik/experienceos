aa1982ce8e90ea6558d4d643a0f06595744f0ad0
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-05-31 19:46:48 +0530

    disabled element selection ability

--- src/app/macos/page.tsx
+++ src/app/macos/page.tsx
@@ -3,12 +3,12 @@ import Menubar from "./components/Menubar";
 import Dock from "./components/Dock";
 
 const page = () => {
-  return (
-    <div className="macos h-[100vh] w-[100w] flex flex-col justify-between ">
-      <Menubar />
-      <Dock />
-    </div>
-  );
+	return (
+		<div className="macos h-[100vh] w-[100w] flex flex-col justify-between select-none">
+			<Menubar />
+			<Dock />
+		</div>
+	);
 };
 
 export default page;
