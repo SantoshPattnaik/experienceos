fdeabb435e896c0dab55be28f261de4eba67dba1
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-06-01 21:59:37 +0530

    refactored location of FullScreenButton Component for import

--- src/app/main_interface/main.tsx
+++ src/app/main_interface/main.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import FullScreenButton from "../components/FullScreenButton";
+import FullScreenButton from "./components/FullScreenButton";
 
 const Main = (): React.JSX.Element => {
 	return (
