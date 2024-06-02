1cc837085330216dd2471971c0f4592bf1e2f2f3
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-06-02 22:54:25 +0530

    some formatting

--- src/app/macos/components/LaunchPad.tsx
+++ src/app/macos/components/LaunchPad.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function LaunchPad() {
-  return <div className=" w-[100vw] h-[645px] bg-white/40">LaunchPad</div>;
+	return <div className=" w-[100vw] h-[645px] bg-white/40">LaunchPad</div>;
 }
 
 export default LaunchPad;
