a0fb4cd3458f35157005bea3db4e4da986502dc5
Alter2k20 <mishraashutosh98611@gmail.com>
2024-06-02 22:53:45 +0530

    added more styling and moved dock and menubar into desktoop in macos

--- src/app/macos/components/LaunchPad.tsx
+++ src/app/macos/components/LaunchPad.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+// import Discord from "../../../../public/macosicons/MacosICO/CleanMyMac .svg"
+// import Dropbox from "../../../../public/macosicons/MacosICO/Dropbox.svg"
+// import Dropbox from "../../../../public/macosicons/MacosICO/FireFox.svg"
+// import Dropbox from "../../../../public/macosicons/MacosICO/JetBrains.svg"
+// import Dropbox from "../../../../public/macosicons/MacosICO/Notion - dark.svg"
+// import Dropbox from "../../../../public/macosicons/MacosICO/Dropbox.svg"
+// import Dropbox from "../../../../public/macosicons/MacosICO/Dropbox.svg"
+
 function LaunchPad() {
-  return <div className=" w-[100vw] h-[645px] bg-white/40">LaunchPad</div>;
+  return <div className=" w-[100vw] h-full bg-white/40">LaunchPad</div>;
 }
 
 export default LaunchPad;
