fe55b9321f9bcf75e56a294f2b9de26bb8d9822c
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-06-02 23:24:21 +0530

    expected merge conflict

--- src/app/macos/components/Dock.tsx
+++ src/app/macos/components/Dock.tsx
@@ -12,51 +12,66 @@ import appLauncher from "../../../../public/macosicons/png-clipart-launchpad-com
 import LaunchPad from "./LaunchPad";
 
 function Dock() {
-  const [launchPad, setLaunchPad] = useState("");
-  return (
-    <div className="flex flex-col align-middle place-items-center">
-      <div className="bottom-[70px] absolute h-[87%]">
-        {/* Launchpad */}
-        {launchPad == "open" ? <LaunchPad /> : ""}
-      </div>
-      <div className=" bg-white/50 w-fit h-[60px] self-center rounded-[25px] pr-3 pl-3 bottom-2 absolute">
-        <div className=" flex justify-center">
-          <ul className="flex">
-            {/*add more icons here */}
-            <li>
-              <Image src={calender} alt="" width={64} height={64} />
-            </li>
-            <li>
-              <Image src={downloads} alt="" width={64} height={64} />
-            </li>
-            <li>
-              <Image src={xcode} alt="" width={64} height={64} />
-            </li>
-            <li>
-              <Image src={finder} alt="" width={64} height={64} />
-            </li>
-            <li>
-              <Image src={folder} alt="" width={64} height={64} />
-            </li>
-            <li>
-              <button
-                className="outline-none mb-2"
-                onClick={() => {
-                  if (launchPad == "") {
-                    setLaunchPad("open");
-                  } else {
-                    setLaunchPad("");
-                  }
-                }}
-              >
-                <Image src={appLauncher} alt="" width={60} height={60} />
-              </button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+	const [launchPad, setLaunchPad] = useState("");
+	return (
+		<div className="flex flex-col align-middle place-items-center ">
+			<div className="bottom-[70px] absolute">
+				{/* Launchpad */}
+				{launchPad == "open" ? <LaunchPad /> : ""}
+			</div>
+			<div className=" bg-white/50 w-fit h-[60px] self-center rounded-[25px] pr-3 pl-3 bottom-2 absolute">
+				<div className=" flex justify-center h-[87%]">
+					<ul className="flex">
+						{/*add more icons here */}
+						<li>
+							<Image
+								src={calender}
+								alt=""
+								width={64}
+								height={64}
+							/>
+						</li>
+						<li>
+							<Image
+								src={downloads}
+								alt=""
+								width={64}
+								height={64}
+							/>
+						</li>
+						<li>
+							<Image src={xcode} alt="" width={64} height={64} />
+						</li>
+						<li>
+							<Image src={finder} alt="" width={64} height={64} />
+						</li>
+						<li>
+							<Image src={folder} alt="" width={64} height={64} />
+						</li>
+						<li>
+							<button
+								className="outline-none mb-2"
+								onClick={() => {
+									if (launchPad == "") {
+										setLaunchPad("open");
+									} else {
+										setLaunchPad("");
+									}
+								}}
+							>
+								<Image
+									src={appLauncher}
+									alt=""
+									width={60}
+									height={60}
+								/>
+							</button>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Dock;
