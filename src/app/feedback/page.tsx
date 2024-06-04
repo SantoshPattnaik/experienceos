8fef1c3b81c71e4b654b2ed47a14109bed414d2d
Alter2k20 <96902776+Alter2k20@users.noreply.github.com>
2024-06-04 19:14:59 +0530

    added some styling and a  feedbac form

---
+++ src/app/feedback/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FeedbackForm from './components/FeedbackForm'
+
+function page() {
+    return (
+        <div className='h-screen w-screen'><FeedbackForm /></div>
+    )
+}
+
+export default page
