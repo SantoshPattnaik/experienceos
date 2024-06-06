76014a2b3b83ead1f287bb9efd4e85874038de2b
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-06-06 20:20:23 +0530

    tooltip react component

---
+++ src/app/windows/components/Tooltip.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Tooltip = () => {
+	return (
+		<>
+			<div className="absolute w-fit bg-white text-black p-1 ml-10 mt-9">
+				<p>Date Created: bhsfjhsdvfjsd</p>
+				<p>Empty Folder</p>
+			</div>
+		</>
+	);
+};
+
+export default Tooltip;
