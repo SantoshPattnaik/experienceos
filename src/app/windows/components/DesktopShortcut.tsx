4dbfe117badb05283778dc8f22022587c46a0ce7
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-06-06 20:20:02 +0530

    added tooltip support

--- src/app/windows/components/DesktopShortcut.tsx
+++ src/app/windows/components/DesktopShortcut.tsx
@@ -1,36 +1,53 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 import folder from "../../../../public/windowsicons/folder.png";
 import thispc from "../../../../public/windowsicons/monitor.png";
 import recycle from "../../../../public/windowsicons/icons8-recycle-bin-96.png";
 import controlp from "../../../../public/windowsicons/icons8-control-panel-96.png";
+import Tooltip from "./Tooltip";
 
 function DesktopShortcut() {
-  return (
-    <div className="flex flex-col h-fit w-fit p-3 text-white text-sm">
-      <div className="hover:cursor-pointer">
-        <Image src={folder} alt="folderico" height={64} width={64} />
-        <label htmlFor="folderico">New folder</label>
-      </div>
-      <div className="hover:cursor-pointer">
-        <Image src={thispc} alt="pcico" height={64} width={64} />
-        <label htmlFor="pcico">This PC</label>
-      </div>
-      <div className="hover:cursor-pointer">
-        <Image src={controlp} alt="panico" height={64} width={64} />
-        <label htmlFor="panico">Control Panel</label>
-      </div>
-      <div className="hover:cursor-pointer">
-        <Image src={recycle} alt="binico" height={64} width={64} />
-        <label htmlFor="binico">Recycle Bin</label>
-      </div>
-      <div className="hover:cursor-pointer">
-        <Image src={folder} alt="folderico" height={64} width={64} />
-        <label htmlFor="folderico">jai kala</label>
-      </div>
-    </div>
-  );
+	const [isShowing, setIsShowing] = useState<boolean>(false);
+
+	return (
+		<div className="flex flex-col h-fit w-fit p-3 text-white text-sm">
+			<div
+				className="hover:cursor-pointer"
+				onMouseOver={() => {
+					if (!isShowing) {
+						setIsShowing(true);
+					}
+					console.log("hover working");
+				}}
+				onMouseOut={() => {
+					setIsShowing(false);
+				}}
+			>
+				<Image src={folder} alt="folderico" height={64} width={64} />
+				<label htmlFor="folderico">New folder</label>
+			</div>
+			{isShowing && <Tooltip />}
+			<div className="hover:cursor-pointer">
+				<Image src={thispc} alt="pcico" height={64} width={64} />
+				<label htmlFor="pcico">This PC</label>
+			</div>
+			<div className="hover:cursor-pointer">
+				<Image src={controlp} alt="panico" height={64} width={64} />
+				<label htmlFor="panico">Control Panel</label>
+			</div>
+			<div className="hover:cursor-pointer">
+				<Image src={recycle} alt="binico" height={64} width={64} />
+				<label htmlFor="binico">Recycle Bin</label>
+			</div>
+			<div className="hover:cursor-pointer">
+				<Image src={folder} alt="folderico" height={64} width={64} />
+				<label htmlFor="folderico">jai kala</label>
+			</div>
+		</div>
+	);
 }
 
 export default DesktopShortcut;
