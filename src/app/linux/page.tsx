1812a7b65a8b1f8bd32a621d97996c64d04522dd
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-06-06 20:53:13 +0530

    removed automatic text select feature

--- src/app/linux/page.tsx
+++ src/app/linux/page.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import GNOME from "./components/GNOME";
 
 const page = () => {
-  return (
-    <div className="linux h-[100vh] w-[100vw] flex flex-col">
-      <GNOME />
-    </div>
-  );
+	return (
+		<div className="linux h-[100vh] w-[100vw] flex flex-col select-none">
+			<GNOME />
+		</div>
+	);
 };
 
 export default page;
