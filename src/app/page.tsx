e32622ad100daffe042d40aee0cbdf7f795ab049
Alter2k20 <mishraashutosh98611@gmail.com>
2024-06-09 20:13:24 +0530

    testing animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Main from "./main_interface/main";
 
 const Home = (): React.JSX.Element => {
 	return (
-		<div className="h-full w-full select-none">
+		<div className="h-full w-full select-none ">
 			<Main />
 		</div>
 	);
--- src/app/windows/components/StartMenu.tsx
+++ src/app/windows/components/StartMenu.tsx
@@ -1,10 +1,10 @@
 function StartMenu() {
-  return (
-    <div className="text-white h-[665px] w-[605px] absolute bottom-[70px] bg-black/50 rounded-xl ml-[500px] backdrop-blur-md backdrop-opacity-80">
-      {" "}
-      this is the startmenu
-    </div>
-  );
+	return (
+		<div className="wndows_AnimateFromBottom text-white h-[665px] w-[605px] absolute bottom-[70px] bg-black/50 rounded-xl ml-[500px] backdrop-blur-md backdrop-opacity-80">
+			{" "}
+			this is the startmenu
+		</div>
+	);
 }
 
 export default StartMenu;
