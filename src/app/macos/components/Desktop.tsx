5b0eb4d8ab3499aa5f717dca7f11347c5b625ca5
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-06-10 18:20:09 +0530

    loading screen implemented successfully

--- src/app/macos/components/Desktop.tsx
+++ src/app/macos/components/Desktop.tsx
@@ -4,12 +4,18 @@ import Menubar from "./Menubar";
 import Dock from "./Dock";
 
 function Desktop() {
-  return (
-    <div>
-      <Menubar />
-      <Dock />
-    </div>
-  );
+	return (
+		<div
+			style={{
+				backgroundImage: "url('366398.jpg')",
+				backgroundSize: "cover",
+			}}
+			className="h-full"
+		>
+			<Menubar />
+			<Dock />
+		</div>
+	);
 }
 
 export default Desktop;
--- src/app/macos/components/LoadingScreen.tsx
+++ src/app/macos/components/LoadingScreen.tsx
@@ -1,19 +1,40 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Desktop from "./Desktop";
 
 const LoadingScreen = () => {
 	const [welcome, setWelcome] = useState<boolean>(false);
 
-	return (
-		<div className="bg-black h-full flex justify-center">
-			<video width={700} autoPlay muted>
-				<source src="/macosicons/large.mp4" type="video/mp4" />
-			</video>
+	const videoEnd = () => {
+		setTimeout(() => {
+			setWelcome(true);
+		}, 9000);
+	};
 
-			{/* <Desktop /> */}
-		</div>
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setWelcome(true);
+		}, 9000);
+		return () => clearTimeout(timer);
+	}, []);
+
+	return (
+		<>
+			<div className={`${welcome ? "h-full visible" : "hidden"}`}>
+				<Desktop />
+			</div>
+
+			<div
+				className={`bg-black h-full flex justify-center${
+					welcome ? "hidden" : ""
+				}`}
+			>
+				<video width={700} autoPlay muted onEnded={videoEnd}>
+					<source src="/macosicons/large.mp4" type="video/mp4" />
+				</video>
+			</div>
+		</>
 	);
 };
 
