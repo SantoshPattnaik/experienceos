aa762f7ba846aadf338f2697dfa96c672cab29f4
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-06-12 12:52:10 +0530

    changed windows font family to default 'Segoe UI'

--- src/app/windows/components/Desktop.tsx
+++ src/app/windows/components/Desktop.tsx
@@ -3,14 +3,9 @@ import React from "react";
 import DesktopShortcut from "./DesktopShortcut";
 import Footer from "./Footer";
 
-// import folder from "../../../../public/windowsicons/folder.png";
-// import thispc from "../../../../public/windowsicons/monitor.png";
-// import recycle from "../../../../public/windowsicons/icons8-recycle-bin-96.png";
-// import controlp from "../../../../public/windowsicons/icons8-control-panel-96.png";
-
 function Desktop() {
 	return (
-		<div className="h-screen w-screen">
+		<div className="h-screen w-screen" style={{ fontFamily: "Segoe UI" }}>
 			<DesktopShortcut />
 			<Footer />
 		</div>
