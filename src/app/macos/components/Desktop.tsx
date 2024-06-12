dc892c5bfce311f1abb19ac890ddf7bc0d080706
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-06-12 23:01:50 +0530

    wrapped the Desktop components with MenuContextProvider

--- src/app/macos/components/Desktop.tsx
+++ src/app/macos/components/Desktop.tsx
@@ -3,20 +3,22 @@ import React from "react";
 import Menubar from "./Menubar";
 import Dock from "./Dock";
 import { AppleFontMedium, AppleFontRegular } from "@/app/fonts";
+import { MenuContextProvider } from "@/app/context/MenuContextProvider";
 
 function Desktop() {
 	return (
-		<div
-			style={{
-				backgroundImage: "url('366398.jpg')",
-				backgroundSize: "cover",
-			}}
-			// className={`${AppleFontRegular.className} h-full`}
-			className={`${AppleFontMedium.className} h-full`}
-		>
-			<Menubar />
-			<Dock />
-		</div>
+		<MenuContextProvider>
+			<div
+				style={{
+					backgroundImage: "url('366398.jpg')",
+					backgroundSize: "cover",
+				}}
+				className={`${AppleFontMedium.className} h-full`}
+			>
+				<Menubar />
+				<Dock />
+			</div>
+		</MenuContextProvider>
 	);
 }
 
