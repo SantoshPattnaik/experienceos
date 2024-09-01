e30b2ebacb8983d36450caf09874de83857ecf49
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-09-01 19:49:28 +0530

    renamed and type annotation changed

--- src/app/types/index.d.ts
+++ src/app/types/index.d.ts
@@ -1,4 +1,8 @@
-interface MenuContextProps {
-    menu: string;
-    setMenu: Dispatch<SetStateAction<string>>;
+interface MenuBarCloseContextProps {
+    close: boolean;
+    setClose: Dispatch<SetStateAction<boolean>>;
+}
+interface SettingsContextProps {
+    render: boolean;
+    setRender: Dispatch<SetStateAction<boolean>>;
 }
