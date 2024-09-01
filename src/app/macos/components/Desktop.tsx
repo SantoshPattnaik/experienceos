9c71f156a194c16edac6953c8ccc926aa4c6e58d
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-09-01 20:03:23 +0530

    removed the commented code of context usage

--- src/app/macos/components/Desktop.tsx
+++ src/app/macos/components/Desktop.tsx
@@ -8,7 +8,6 @@ import { SettingsContextProvider } from "@/app/context/SettingsContextProvider";
 import Application_renderer from "./Application_renderer";
 
 function Desktop() {
-	// const render = useContext(SettingsContext) as SettingsContextProps;
 	return (
 		<MenuBarCloseContextProvider>
 			<div
