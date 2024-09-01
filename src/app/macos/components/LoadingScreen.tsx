dfc188177d3ec453a8b7c69576037966c2c4ae3a
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-09-01 19:56:23 +0530

    some formattings

--- src/app/macos/components/LoadingScreen.tsx
+++ src/app/macos/components/LoadingScreen.tsx
@@ -21,28 +21,16 @@ const LoadingScreen = () => {
 
 	return (
 		<>
-			<div
-				className={`${
-					welcome ? "h-full visible macos_fadein" : "hidden"
-				}`}
-			>
+			<div className={`${welcome ? "h-full visible macos_fadein" : "hidden"}`}>
 				<Desktop />
 			</div>
 
-			<div
-				className={`${
-					welcome
-						? "hidden"
-						: "bg-black h-full w-full flex justify-center"
-				}`}
-			>
+			<div className={`${welcome ? "hidden" : "bg-black h-full w-full flex justify-center"}`}>
 				<video width={200} autoPlay muted onEnded={videoEnd}>
 					<source src="/macosicons/large.mp4" type="video/mp4" />
 				</video>
 				<div className="outline-1 rounded-lg h-2 w-[80px] text-white">
-					<div className="loadbar bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-						{" "}
-					</div>
+					<div className="loadbar bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"> </div>
 				</div>
 			</div>
 		</>
