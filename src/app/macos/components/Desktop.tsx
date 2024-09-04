648ed0b7754b9a23121e1ccc87ad1cb9c71fccb5
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2024-09-04 12:42:12 +0530

    added CSS in JS code for css functionality

--- src/app/macos/components/Desktop.tsx
+++ src/app/macos/components/Desktop.tsx
@@ -5,23 +5,34 @@ import { AppleFontMedium, AppleFontRegular } from "@/app/fonts";
 import { MenuBarCloseContextProvider } from "@/app/context/MenuBarCloseContextProvider";
 import { SettingsContextProvider } from "@/app/context/SettingsContextProvider";
 import Application_renderer from "./Application_renderer";
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+
+const Animation = keyframes`
+	from {
+		opacity:0;
+	}
+	to {
+		opacity:1;
+	}
+`;
+
+const FadeIn = styled.div`
+	animation: ${Animation} 1s Linear;
+	background-image: url(366398.jpg);
+	background-size: cover;
+`;
 
 function Desktop() {
 	return (
 		<MenuBarCloseContextProvider>
-			<div
-				style={{
-					backgroundImage: "url('366398.jpg')",
-					backgroundSize: "cover",
-				}}
-				className={`${AppleFontMedium.className} h-full`}
-			>
+			<FadeIn className={`${AppleFontMedium.className} h-full`}>
 				<SettingsContextProvider>
 					<Menubar />
 					<Application_renderer />
 				</SettingsContextProvider>
 				<Dock />
-			</div>
+			</FadeIn>
 		</MenuBarCloseContextProvider>
 	);
 }
